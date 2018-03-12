efa5ee2e74a4a5db36d024353f4d026191c46fc0
Shubham Jain <cs1160317@cse.iitd.ac.in>
2018-03-12 14:01:27 +0530

    Updated ReadMe and changed Main

--- COP290/2Dto3D.h
+++ COP290/2Dto3D.h
@@ -12,7 +12,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
-
+#include <math.h>
 
 
 void viewtype(TwoDBody twodbody)
--- COP290/Main.cpp
+++ COP290/Main.cpp
@@ -5,9 +5,34 @@
 #include "Reduce2DInput.h"
 #include "2Dto3D.h"
 #include "Recreate3DOutput.h"
-int main(std::ifstream &InputFile)
-{
-	return 0;
+// GUI interface
+
+void display() {
+
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
+	glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer
+ 
+	glBegin(GL_QUADS);              // Each set of 4 vertices form a quad
+    glColor3f(0.0f, 0.0f, 1.0f); // Blue
+    glVertex2f(-0.5f, -0.5f);    // x, y
+    glVertex2f( 0.4f, -0.6f);
+    glVertex2f( 0.6f,  0.4f);
+    glVertex2f(-0.5f,  0.5f);
+	glEnd();
+ 
+	glFlush();  // Render now
 };
 
+//int main(std::ifstream &InputFile)
+int main(int argc, char** argv){
+
+	glutInit(&argc, argv);                 // Initialize GLUT
+	glutCreateWindow("OpenGL Setup Test"); // Create a window with the given title
+	glutInitWindowSize(640, 320);   // Set the window's initial width & height
+	glutInitWindowPosition(100, 50); // Position the window's initial top-left corner
+	glutDisplayFunc(display); // Register display callback handler for window re-paint
+	glutMainLoop();           // Enter the infinitely event-processing loop
+	return 0;
+
+};
 	//encapsulates all the functions involving 3d to 2d projection.
